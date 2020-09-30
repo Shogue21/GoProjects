fbe47420e38148058d4fb9fdda020876e633488e
Seth Hogue <shogue21@basecampcodingacademy.org>
2020-09-30 10:53:31 -0500

    Transfered book.go benchmark to GoProjects folder

---
+++ books.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	newBookCost = 6
+	oldBookCost = 3.5
+)
+
+func computeCost(new, old float64) float64 {
+	return new*newBookCost + old*oldBookCost
+}
+
+func applyCouponDiscount(cost float64, coupon string) float64 {
+	if coupon == "free-stuff" {
+		return 0
+	} else if coupon == "half-off" {
+		return cost * 0.5
+	} else {
+		return cost
+	}
+}
+
+func isValidCoupon(coupon string) bool {
+	return bool(coupon == "free-stuff" || coupon == "half-off")
+}
+func main() {
+
+	fmt.Println("Welcome to Bargain Books!")
+	fmt.Println("New books are $", newBookCost, "each.")
+	fmt.Println("Old books are $", oldBookCost, "each.")
+
+	fmt.Println("How many new books are you buying today?")
+	var newBooks int
+	fmt.Scanln(&newBooks)
+
+	fmt.Println("How many old books are you buying today?")
+	var oldBooks int
+	fmt.Scanln(&oldBooks)
+	total := computeCost(float64(newBooks), float64(oldBooks))
+
+	fmt.Println("Do you have a coupon? [Y/N]: ")
+	var hasCoupon string
+	fmt.Scanln(&hasCoupon)
+	hasCoupon = strings.ToLower(hasCoupon)
+
+	if hasCoupon == "y" {
+		fmt.Println("What is your coupon? ")
+		var coupon string
+		fmt.Scanln(&coupon)
+		if isValidCoupon(coupon) {
+			total = applyCouponDiscount(total, coupon)
+			fmt.Println("Coupon successfully applied!")
+		} else {
+			fmt.Println("I'm sorry, that coupon code is not valid.")
+		}
+	}
+
+	fmt.Println("That will be $", total)
+}
